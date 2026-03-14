875a9e32badf328ae84ef0c6d26c8db3b92b78c8
svanier-droid <svanier@silverstar.ca>
2026-03-14 09:12:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
 
 const app = express();
-app.use(express.json({ limit: '25mb' }));
+app.use(express.json({ limit: '30mb' }));
 
 app.get('/', (req, res) => {
   res.send('Silver Star PDF signer is running.');
@@ -15,11 +15,12 @@ app.post('/sign', async (req, res) => {
       signatureDataUrl,
       signedDateText,
       x = 72,
-      y = 24,
+      y = 45,
       width = 150,
       height = 34,
       dateX = 128,
-      dateY = 22
+      dateY = 40,
+      certificate = {}
     } = req.body;
 
     if (!pdfBase64) {
@@ -48,6 +49,8 @@ app.post('/sign', async (req, res) => {
     });
 
     const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    const boldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+
     lastPage.drawText(signedDateText || '', {
       x: dateX,
       y: dateY,
@@ -55,6 +58,73 @@ app.post('/sign', async (req, res) => {
       font,
       color: rgb(0, 0, 0)
     });
+
+    const certPage = pdfDoc.addPage([612, 792]);
+
+    certPage.drawText('Electronic Signature Certificate', {
+      x: 50,
+      y: 740,
+      size: 18,
+      font: boldFont,
+      color: rgb(0, 0, 0)
+    });
+
+    certPage.drawText(certificate.companyName || 'Silver Star Mercedes-Benz', {
+      x: 50,
+      y: 715,
+      size: 11,
+      font,
+      color: rgb(0, 0, 0)
+    });
+
+    const lines = [
+      ['Request ID', certificate.requestId || ''],
+      ['RO', certificate.ro || ''],
+      ['VIN', certificate.vin || ''],
+      ['Client', certificate.clientName || ''],
+      ['Advisor', certificate.advisorName || ''],
+      ['Signed By', certificate.signedByName || ''],
+      ['Signed Date', certificate.signedDateText || signedDateText || ''],
+      ['IP Address', certificate.signedIp || ''],
+      ['Device Info', certificate.deviceInfo || ''],
+      ['Timezone', certificate.timezone || ''],
+      ['Consent Confirmed', certificate.consent || 'YES']
+    ];
+
+    let yPos = 670;
+    for (const [label, value] of lines) {
+      certPage.drawText(`${label}:`, {
+        x: 50,
+        y: yPos,
+        size: 11,
+        font: boldFont,
+        color: rgb(0, 0, 0)
+      });
+
+      const safeValue = String(value || '');
+      certPage.drawText(safeValue, {
+        x: 180,
+        y: yPos,
+        size: 11,
+        font,
+        color: rgb(0, 0, 0),
+        maxWidth: 370
+      });
+
+      yPos -= 28;
+    }
+
+    certPage.drawText(
+      'This certificate was generated automatically at the time of electronic signature submission.',
+      {
+        x: 50,
+        y: 90,
+        size: 10,
+        font,
+        color: rgb(0.25, 0.25, 0.25),
+        maxWidth: 500
+      }
+    );
 
     const signedPdfBytes = await pdfDoc.save();
     const signedPdfBase64 = Buffer.from(signedPdfBytes).toString('base64');
@@ -64,7 +134,7 @@ app.post('/sign', async (req, res) => {
       signedPdfBase64
     });
   } catch (err) {
-    console.error(err);
+    console.error('SIGN ERROR:', err);
     res.status(500).json({
       error: err.message || String(err)
     });
